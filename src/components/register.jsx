31ae613272c8f4c7ce69b92e6d4072dab04d81fc
yogesh-d-be <yogesh.d.be@gmail.com>
2024-06-12 17:40:22 +0530

    changes env

--- src/components/register.jsx
+++ src/components/register.jsx
@@ -7,20 +7,18 @@ import 'react-toastify/dist/ReactToastify.css';
 import '../components/register.css';
 
 function Register(){
+    const [isSignedUp, setIsSignedUp] = useState(false);
+    const [token, setToken] = useState(localStorage.getItem("token") || null);
 
-    const [isSignedUp, setIsSignedUp] = useState(false)
-    const [token, setToken] = useState(localStorage.getItem("token")||null);
-
-    const handleSignUp = () =>{
-        setIsSignedUp(!isSignedUp)     
-               
+    const handleSignUp = () => {
+        setIsSignedUp(!isSignedUp);
     }
 
     const validation = (elements) => {
-      const name = elements.name.value;
-      const email = elements.email.value;
-      const MobileNumber = elements.MobileNumber.value;
-      const password = elements.password.value;
+      const name = elements.name.value.trim();
+      const email = elements.email.value.trim();
+      const mobileNumber = elements.MobileNumber.value.trim();
+      const password = elements.password.value.trim();
       
       if (name === "") {
         toast.error("Enter Your Name");
@@ -31,10 +29,10 @@ function Register(){
       } else if (!email.includes("@") || !email.includes(".")) {
         toast.error("Enter a Valid Email");
         return false;
-      } else if (MobileNumber === "") {
+      } else if (mobileNumber === "") {
         toast.error("Enter Your Mobile Number");
         return false;
-      } else if (MobileNumber.length !== 10 || isNaN(MobileNumber)) {
+      } else if (mobileNumber.length !== 10 || isNaN(mobileNumber)) {
         toast.error("Enter a Valid Mobile Number");
         return false;
       } else if (password === "") {
@@ -43,15 +41,16 @@ function Register(){
       }
       return true;
     };
-  
+
     const apiUrl = process.env.REACT_APP_API_URL;
-  
+
     const handleRegister = async (event) => {
       event.preventDefault();
       const elements = event.target.elements;
       if (!validation(elements)) return;
-      const { name, email, MobileNumber, password } = event.target.elements;
-    
+
+      const { name, email, MobileNumber, password } = elements;
+
       try {
         const response = await axios.post(`${apiUrl}/userregister`, {
           name: name.value,
@@ -59,10 +58,10 @@ function Register(){
           MobileNumber: MobileNumber.value,
           password: password.value,
         });
-  
+
         if (response.status === 201) {
           toast.success('Registration successful!');
-          event.target.reset();  // Reset the form fields
+          event.target.reset();  
           handleSignUp();
         }
       } catch (error) {
@@ -74,11 +73,11 @@ function Register(){
         console.error('Error during registration', error);
       }
     };
-  
+
     const handleLogin = async (event) => {
       event.preventDefault();
       const { email, password } = event.target.elements;
-      if (!email.value || !password.value) {
+      if (!email.value.trim() || !password.value.trim()) {
         toast.error('Please fill in all fields');
         return;
       }
@@ -89,7 +88,7 @@ function Register(){
         });
         toast.success('Login successful!');
         const receivedToken = response.data.data;
-        setToken(receivedToken);  // Store the token in the state
+        setToken(receivedToken);  
         localStorage.setItem("token", receivedToken);
         console.log('Login successful', response.data);
       } catch (error) {
@@ -104,87 +103,89 @@ function Register(){
         console.error('Login failed', error);
       }
     };
+
     useEffect(() => {
       const storedToken = localStorage.getItem("token");
       if (storedToken) {
         setToken(storedToken);
+        console.log("token",token)
       }
-    }, []);
+     
+    }, [token]);
 
-    return(
+    return (
         <>
-         <ToastContainer />
-        <div className={`container ${isSignedUp?"sign-up-mode":''}`} >
-            <div className="forms-container">
-                <div className="signin-signup">
-                <form action="" className="sign-in-form"  onSubmit={handleLogin}>
-                    <div className="form-field">
-                    <h2 className="title">Sign In</h2>
-                    <div className="input-field">
-                        <span className=""><FontAwesomeIcon icon={faEnvelope} /></span>
-                        <input type="text" name="email" placeholder="Email ID"  />
-                    </div>
-                    <div className="input-field">
-                        <span className=""><FontAwesomeIcon icon={faLock} /></span>
-                        <input type="password" name="password" placeholder="Password"  />
-                    </div>
-                    <div className="text-center">
-                    <input type="submit" value="Login" className="btn"/>
-                    <p className="form-b-content text-gray-600 hover:text-blue-700 hover:font-bold pass">Forgot Password?</p>
-                    <p className="form-b-content  text-gray-600">Create Account? <span onClick={handleSignUp} className="text-blue-700 font-bold">Sign Up</span></p>
-                    </div>
-                    </div>
-                </form>
+          <ToastContainer />
+          <div className={`container ${isSignedUp ? "sign-up-mode" : ''}`}>
+              <div className="forms-container">
+                  <div className="signin-signup">
+                      <form className="sign-in-form" onSubmit={handleLogin}>
+                          <div className="form-field">
+                              <h2 className="title">Sign In</h2>
+                              <div className="input-field">
+                                  <span className=""><FontAwesomeIcon icon={faEnvelope} /></span>
+                                  <input type="text" name="email" placeholder="Email ID" />
+                              </div>
+                              <div className="input-field">
+                                  <span className=""><FontAwesomeIcon icon={faLock} /></span>
+                                  <input type="password" name="password" placeholder="Password" />
+                              </div>
+                              <div className="text-center">
+                                  <input type="submit" value="Login" className="btn"/>
+                                  <p className="form-b-content text-gray-600 hover:text-blue-700 hover:font-bold pass">Forgot Password?</p>
+                                  <p className="form-b-content text-gray-600">Create Account? <span onClick={handleSignUp} className="text-blue-700 font-bold">Sign Up</span></p>
+                              </div>
+                          </div>
+                      </form>
 
-                <form action="" className="sign-up-form" onSubmit={handleRegister}>
-                <div className="form-field up">
-                    <h2 className="title">Sign Up</h2>
-                    <div className="input-field">
-                        <span className=""><FontAwesomeIcon icon={faUser} /></span>
-                        <input type="text" name="name" placeholder="Fullname"  />
-                    </div>
-                    <div className="input-field">
-                        <span className=""><FontAwesomeIcon icon={faEnvelope} /></span>
-                        <input type="text" name="email" placeholder="Email ID"  />
-                    </div>
-                    <div className="input-field">
-                        <span className=""><FontAwesomeIcon icon={faMobile} /></span>
-                        <input type="text" name="MobileNumber" placeholder="Mobile Number"  />
-                    </div>
-                    <div className="input-field">
-                        <span className=""><FontAwesomeIcon icon={faLock} /></span>
-                        <input type="password" name="password" placeholder="Password"  />
-                    </div>
-                    <div className="text-center">
-                    <input type="submit" value="Register" className="btn"/>
-                    <p className="form-b-content text-gray-600">Already have an Account? <span onClick={handleSignUp} className="text-blue-700 font-bold">Sign In</span></p>
-                    </div>
-                    </div>
-                </form>
-                </div>
-            </div>
+                      <form className="sign-up-form" onSubmit={handleRegister}>
+                          <div className="form-field up">
+                              <h2 className="title">Sign Up</h2>
+                              <div className="input-field">
+                                  <span className=""><FontAwesomeIcon icon={faUser} /></span>
+                                  <input type="text" name="name" placeholder="Fullname" />
+                              </div>
+                              <div className="input-field">
+                                  <span className=""><FontAwesomeIcon icon={faEnvelope} /></span>
+                                  <input type="text" name="email" placeholder="Email ID" />
+                              </div>
+                              <div className="input-field">
+                                  <span className=""><FontAwesomeIcon icon={faMobile} /></span>
+                                  <input type="text" name="MobileNumber" placeholder="Mobile Number" />
+                              </div>
+                              <div className="input-field">
+                                  <span className=""><FontAwesomeIcon icon={faLock} /></span>
+                                  <input type="password" name="password" placeholder="Password" />
+                              </div>
+                              <div className="text-center">
+                                  <input type="submit" value="Register" className="btn"/>
+                                  <p className="form-b-content text-gray-600">Already have an Account? <span onClick={handleSignUp} className="text-blue-700 font-bold">Sign In</span></p>
+                              </div>
+                          </div>
+                      </form>
+                  </div>
+              </div>
 
+              <div className="panel-container">
+                  <div className={`panel left-panel ${isSignedUp ? '' : 'active'}`}>
+                      <div className="content">
+                          <h2>New Here?</h2>
+                          <p>Welcome to our job portal! 🌟 Sign Up and explore a world of possibilities!</p>
+                          <button onClick={handleSignUp} className="btn-s" id="sign-up-btn">Sign Up</button>
+                      </div>
+                      <img src={require('../assets/career_progres.png')} alt="career" className="image" />
+                  </div>
 
-            <div className="panel-container">
-                <div className={`panel left-panel `}>
-                    <div className={`content `}>
-                        <h2>New Here?</h2>
-                        <p>Welcome to our job portal! 🌟 Sign Up and explore a world of possibilities!</p>
-                        <button onClick={handleSignUp} className="btn-s" id="sign-up-btn">Sign Up</button>
-                    </div>
-                    <img src={require('../assets/career_progres.png')} alt="career" className={`image `}/>
-                </div>
-
-                <div className="panel right-panel">
-                    <div className="content">
-                        <h2>One Of Us?</h2>
-                        <p>Welcome back! 🌟 Explore new opportunities, connect with employers, and take your career to the next level. Let’s find your dream job together!</p>
-                        <button onClick={handleSignUp} className="btn-s" id="sign-in-btn">Sign In</button>
-                    </div>
-                    <img src={require('../assets/Job_hunt.png')} alt="pick career" className="image"/>
-                </div>
-            </div>
-        </div>
+                  <div className={`panel right-panel ${isSignedUp ? 'active' : ''}`}>
+                      <div className="content">
+                          <h2>One Of Us?</h2>
+                          <p>Welcome back! 🌟 Explore new opportunities, connect with employers, and take your career to the next level. Let’s find your dream job together!</p>
+                          <button onClick={handleSignUp} className="btn-s" id="sign-in-btn">Sign In</button>
+                      </div>
+                      <img src={require('../assets/Job_hunt.png')} alt="pick career" className="image" />
+                  </div>
+              </div>
+          </div>
         </>
     )
 }
